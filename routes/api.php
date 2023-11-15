97fa66f05c4e9ca0472484cec535839c1aaa1351
Raihan "Splitcentre" Alfian Pratama <100069684+splitcentre@users.noreply.github.com>
2023-11-15 08:33:47 +0700

    generate api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\GreetController;
+use App\Http\Controllers\InfoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/info', [InfoController::class, 'index'])->name('info');
+Route::get('/greet', [GreetController::class,
+'greet'])->name('greet');
