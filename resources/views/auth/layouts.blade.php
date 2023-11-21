aaeb85211dacd65cc0f9edf531747d72d7cb25e4
Raihan "Splitcentre" Alfian Pratama <100069684+splitcentre@users.noreply.github.com>
2023-11-21 18:29:08 +0700

    tugas done?

--- resources/views/auth/layouts.blade.php
+++ resources/views/auth/layouts.blade.php
@@ -13,7 +13,7 @@
 
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container">
-          <a class="navbar-brand" href="{{ URL('/') }}">Custom Login Register</a>
+          <a class="navbar-brand" href="{{ URL('/') }}">Blog Test</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
